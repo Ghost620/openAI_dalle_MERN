2d257c994cc0d6192c9227f71cc12f62b2d47dd6
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-28 22:35:05 +0500

    generate and share buttons added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -8,10 +8,14 @@ import { FormField, Loader } from '../components'
 const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({name: '', prompt: '', photo: ''})
-  const [generatingImg, setGeneratingImg] = useState(true)
+  const [generatingImg, setGeneratingImg] = useState(false)
   const [loading, setLoading] = useState(false)
 
   const handleSubmit = () => {
+
+  }
+
+  const generateImage = () => {
 
   }
 
@@ -61,6 +65,18 @@ const CreatePost = () => {
           </div>
 
         </div>
+
+        <div className='mt-5 flex gap-5'>
+          <button className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center' type='button' onClick={generateImage}> {generateImage ? 'Generating...' : 'Generate'} </button>
+        </div>
+
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>Once you created the image you want, you can share it with others in the community</p>
+          <button type='submit' className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+            {loading ? 'Sharing...' : 'Share with the community'}
+          </button>
+        </div>
+
       </form>
     </section>
   )
