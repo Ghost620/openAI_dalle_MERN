1d16ef0fe670826fb7d0a8a2aa9bb801b55a8afe
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-30 17:55:40 +0500

    dalle route image response created

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai'
+import { v2 as cloudinary } from 'cloudinary';
+import Post from '../mongodb/models/post.js'
 
 dotenv.config();
 
 const router = express.Router();
+
+
+
+export default router;
