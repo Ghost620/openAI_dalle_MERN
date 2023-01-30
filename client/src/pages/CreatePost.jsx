0483d56bef3390159892951b83ad040a73c1ba2f
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-30 19:39:11 +0500

    picture generated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,17 +15,37 @@ const CreatePost = () => {
 
   }
 
-  const generateImage = () => {
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
 
+        setGeneratingImg(true)
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ prompt: form.prompt })
+        })
+
+        const data = await response.json()
+
+        setForm({...form, photo: `data:image/jpeg;base64,${data.photo}`})
+
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false)
+      }
+    } else { alert('Kindly enter a prompt') }
   }
 
-  const handleChange = (e) => {
-
-  }
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSurpriseMe = () => {
-
-  }
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -38,9 +58,25 @@ const CreatePost = () => {
       <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
 
-          <FormField labelName="Your Name" type="text" name="name" placeholder="John Doe" value={form.name} handleChange={handleChange} />
+        <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Ex., john doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
 
-          <FormField labelName="Prompt" type="text" name="prompt" placeholder="A Space Shuttle flying above Cape Town, digital art" value={form.name} handleChange={handleChange} isSurpriseMe handleSurpriseMe={handleSurpriseMe} />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
 
           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             { form.photo ? (
@@ -66,14 +102,23 @@ const CreatePost = () => {
 
         </div>
 
-        <div className='mt-5 flex gap-5'>
-          <button className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center' type='button' onClick={generateImage}> {generateImage ? 'Generating...' : 'Generate'} </button>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
         </div>
 
-        <div className='mt-10'>
-          <p className='mt-2 text-[#666e75] text-[14px]'>Once you created the image you want, you can share it with others in the community</p>
-          <button type='submit' className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
-            {loading ? 'Sharing...' : 'Share with the community'}
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
         </div>
 
